d32f0bc5d45341b9395aed67579b041ba0631f5c
JoranOut <joranlol@gmail.com>
2024-08-15 07:58:36 +0200

    [GLOBAL] Fix plugin.php

--- soli-featured-image-plugin.php
+++ soli-featured-image-plugin.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace Soli\FeaturedImage;
 
 /*
   Plugin Name: Soli Featured Image Plugin
-  Version: 2.0.1
+  Version: 2.0.2
   Author: Joran Out
 */
 
@@ -11,22 +12,21 @@ require_once 'updater.php';
 require_once 'block/block.php';
 
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
-define( 'SOLI_EVENT__PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
-define( 'SOLI_EVENT__PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
+define('SOLI_EVENT__PLUGIN_DIR_PATH', plugin_dir_path(__FILE__));
+define('SOLI_EVENT__PLUGIN_DIR_URL', plugin_dir_url(__FILE__));
 
 
-add_action( 'init', 'soli_featured_image_plugin_updater_init' );
-function soli_featured_image_plugin_updater_init() {
+add_action('init', function () {
 
   include_once 'updater.php';
 
-  define( 'WP_GITHUB_FORCE_UPDATE', true );
+  define('WP_GITHUB_FORCE_UPDATE', true);
 
-  if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+  if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
 
     $config = array(
-      'slug' => plugin_basename( __FILE__ ), // this is the slug of your plugin
-      'proper_folder_name' => plugin_basename( __FILE__ ), // this is the name of the folder your plugin lives in
+      'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+      'proper_folder_name' => plugin_basename(__FILE__), // this is the name of the folder your plugin lives in
       'api_url' => 'https://api.github.com/repos/JoranOut/wp-soli-featured-image-plugin', // the GitHub API url of your GitHub repo
       'raw_url' => 'https://raw.github.com/JoranOut/wp-soli-featured-image-plugin/master', // the GitHub raw url of your GitHub repo
       'github_url' => 'https://github.com/JoranOut/wp-soli-featured-image-plugin', // the GitHub url of your GitHub repo
@@ -37,8 +37,8 @@ function soli_featured_image_plugin_updater_init() {
       'readme' => 'README.md', // which file to use as the readme for the version number
     );
 
-    new WP_GitHub_Updater( $config );
+    new WP_GitHub_Updater($config);
   }
 
-}
+});
 
